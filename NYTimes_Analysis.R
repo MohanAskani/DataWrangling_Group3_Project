9cc83b0e0863ce93203c0d94a580d9b780a538f7
Naveen <naveen@Computings-MacBook-Pro.local>
2025-05-03 13:33:25 -0400

    Removed unused code and added comments

--- NYTimes_Analysis.R
+++ NYTimes_Analysis.R
@@ -1,6 +1,5 @@
-#Clear R Environment
-rm(list = ls())
 
+# Load required libraries
 library(httr)
 library(jsonlite)
 library(dplyr)
@@ -13,12 +12,10 @@ library(tidyverse)
 library(stopwords)
 library(textstem)
 library(lubridate)
+library(gridExtra)
 
-
-nyt_key <- "3MBRKQHHWDYuuAeX9QpkVd95XlrPBjCR"
-
+# Calling New York Times API through curl
 system('curl -s -o nyt-2025-4.json "https://api.nytimes.com/svc/archive/v1/2025/4.json?api-key=3MBRKQHHWDYuuAeX9QpkVd95XlrPBjCR"')
-
 
 # Adjust path as needed
 json_data <- fromJSON("nyt-2025-4.json", flatten = TRUE)
@@ -26,13 +23,11 @@ json_data <- fromJSON("nyt-2025-4.json", flatten = TRUE)
 # Extract articles
 docs <- json_data$response$docs
 
-glimpse(docs)
-
-dim(docs)
-
-us_news <- docs %>% filter(section_name %in% c('U.S.', 'Sports') & type_of_material == 'News')
+# Extract only US section News 
+us_news <- docs %>% filter(section_name %in% c('U.S.') & type_of_material == 'News')
 dim(us_news)
 
+# Extract body function from page URLs
 body_extraction <- function(url) {
   page <- read_html(url)
   paragraphs <- page %>% html_elements("p") %>% html_text2()
@@ -40,11 +35,12 @@ body_extraction <- function(url) {
   return(clean_text)
 }
 
+# Call body extract function and store output in full_body
 us_news <- us_news %>% mutate(full_body = map_chr(web_url, body_extraction))
 
+# Full body cleaning function starts
 clean_full_body <- function(text) {
   if (is.null(text) || is.na(text) || !is.character(text)) return("")
-  
   cleaned <- text %>%
     gsub("(?:'|’)[sS]\\b", "", .) %>%
     # Remove the specific problematic sentence (all variations)
@@ -78,23 +74,20 @@ clean_full_body <- function(text) {
   return(cleaned)
 }
 
+# Call clean full body function and store output in cleaned_body
 us_news <- us_news %>% mutate(cleaned_body = map_chr(full_body, clean_full_body))
-glimpse(us_news)
-str(us_news)
-cat(us_news$full_body[340])
-cat(us_news$cleaned_body[340])
-summary(nytimes_processed$pub_date)  # Should show proper Date format
+#glimpse(us_news)
+#cat(us_news$full_body[340])
+#cat(us_news$cleaned_body[340])
 
-
-
-# Select and rename the desired columns
+# Select and rename the required columns
 nytimes_processed <- us_news %>%
   select(
     abstract,
     pub_date,
-    publisher = source,        # Use source column as publisher (assuming it's "The New York Times")
-    raw_body = full_body,      # Rename full_body to raw_body
-    cleaned_body               # Keep cleaned_body as is
+    publisher = source,       
+    raw_body = full_body,      
+    cleaned_body            
   ) %>%
   mutate(
     pub_date = as.Date(sub("T.*", "", pub_date)),
@@ -104,7 +97,7 @@ nytimes_processed <- us_news %>%
 
 dim(nytimes_processed)
 # Save to CSV if needed
-write_csv(nytimes_processed, "nytimes_processed.csv")
+#write_csv(nytimes_processed, "nytimes_processed.csv")
 
 tokenisation_standardisation_unigrams <- function(df){
   df <- df %>% 
@@ -114,6 +107,55 @@ tokenisation_standardisation_unigrams <- function(df){
     filter(str_detect(word, "^[a-z]+$")) %>%  # considering only alphabets
     mutate(lemma = lemmatize_words(word)) # Reducing the words to its base form
 }
-
 nytimes_data_tokenized <- tokenisation_standardisation_unigrams(nytimes_processed)
 
+# Topic Coverage Analysis based on Uni-grams
+top_unigrams <- nytimes_data_tokenized %>%
+  count(publisher, lemma, sort = TRUE) %>%
+  group_by(publisher) %>%
+  slice_max(n, n = 15) %>%
+  ungroup()
+p1 <- top_unigrams %>%
+  ggplot(aes(x = reorder_within(lemma, n, publisher), y = n, fill = publisher)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~publisher, scales = "free") +
+  scale_x_reordered() +
+  coord_flip() +
+  labs(title = "Top Unigrams in The New York Times Articles by Publisher (Last 30 Days)",
+       x = "UniGram", y = "Frequency")
+
+tokenisation_standardisation_bigrams <- function(df) {
+  df <- df %>%
+    unnest_tokens(bigram, cleaned_body, token = "ngrams", n = 2) %>%
+    separate(bigram, into = c("word1", "word2"), sep = " ") %>%
+    mutate(
+      word1 = lemmatize_words(word1),
+      word2 = lemmatize_words(word2)
+    ) %>% # Reducing the words to its base form
+    filter(
+      str_length(word1) >= 3, 
+      str_length(word2) >= 3,
+      str_detect(word1, "^[a-z]+$"),
+      str_detect(word2, "^[a-z]+$")
+    ) %>% # Removing words with length < 4 and only words with alphabets
+    anti_join(stop_words, by = c("word1" = "word")) %>% # Handling stopwords
+    anti_join(stop_words, by = c("word2" = "word")) %>% # Handling stopwords
+    unite("bigram", word1, word2, sep = " ")
+}
+nytimes_data_tokenized_bigrams <- tokenisation_standardisation_bigrams(nytimes_processed)
+
+# Topic Coverage Analysis based on Bi-grams
+top_bigrams <- nytimes_data_tokenized_bigrams %>%
+  count(publisher, bigram, sort = TRUE) %>%
+  group_by(publisher) %>%
+  slice_max(n, n = 15) %>%
+  ungroup()
+p2 <- top_bigrams %>%
+  ggplot(aes(x = reorder_within(bigram, n, publisher), y = n, fill = publisher)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~publisher, scales = "free") +
+  scale_x_reordered() +
+  coord_flip() +
+  labs(title = "Top BiGrams in The New York Times Articles by Publisher (Last 30 Days)",
+       x = "BiGram", y = "Frequency")
+grid.arrange(p1, p2, ncol = 2)
